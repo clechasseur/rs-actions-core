97fb0e7fedf01f15a88b2c0903a28c4e61758d61
Charles Lechasseur <shiftingbeard@outlook.com>
2025-01-02 00:02:51 -0500

    chore: fix install tests by adding timeout

--- __tests__/commands/cargoHack.test.ts
+++ __tests__/commands/cargoHack.test.ts
@@ -2,44 +2,58 @@ import * as io from '@actions/io';
 
 import { CargoHack } from '../../src/commands/cargoHack';
 
+const SECONDS = 1000;
+
 describe('CargoHack', () => {
   describe('get', () => {
-    it('fetches the installed cargo-hack', async () => {
-      try {
-        await io.which('cargo-hack', true);
+    it(
+      'fetches the installed cargo-hack',
+      async () => {
+        try {
+          await io.which('cargo-hack', true);
 
-        // cargo-hack is installed, we can test it
-        const cargoHack = await CargoHack.get();
-        const exitCode = await cargoHack.call(['--version']);
-        expect(exitCode).toBe(0);
-      } catch {
-        // Simply skip this test
-        console.log('cargo-hack not installed; skipping this test');
-      }
-    });
+          // cargo-hack is installed, we can test it
+          const cargoHack = await CargoHack.get();
+          const exitCode = await cargoHack.call(['--version']);
+          expect(exitCode).toBe(0);
+        } catch {
+          // Simply skip this test
+          console.log('cargo-hack not installed; skipping this test');
+        }
+      },
+      60 * SECONDS,
+    );
   });
 
   describe('install', () => {
-    it('installs cargo-hack', async () => {
-      try {
-        await io.which('cargo-hack', true);
+    it(
+      'installs cargo-hack',
+      async () => {
+        try {
+          await io.which('cargo-hack', true);
 
-        // Simply skip this test
-        console.log('cargo-hack already installed; skipping this test');
-      } catch {
-        // cargo-hack is not installed; install it
-        const cargoHack = await CargoHack.install();
-        const exitCode = await cargoHack.call(['--version']);
-        expect(exitCode).toBe(0);
-      }
-    });
+          // Simply skip this test
+          console.log('cargo-hack already installed; skipping this test');
+        } catch {
+          // cargo-hack is not installed; install it
+          const cargoHack = await CargoHack.install();
+          const exitCode = await cargoHack.call(['--version']);
+          expect(exitCode).toBe(0);
+        }
+      },
+      60 * SECONDS,
+    );
   });
 
   describe('getOrInstall', () => {
-    it('installs cargo-hack if needed, otherwise reuses it', async () => {
-      const cargoHack = await CargoHack.getOrInstall();
-      const exitCode = await cargoHack.call(['--version']);
-      expect(exitCode).toBe(0);
-    });
+    it(
+      'installs cargo-hack if needed, otherwise reuses it',
+      async () => {
+        const cargoHack = await CargoHack.getOrInstall();
+        const exitCode = await cargoHack.call(['--version']);
+        expect(exitCode).toBe(0);
+      },
+      60 * SECONDS,
+    );
   });
 });
